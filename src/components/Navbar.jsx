72082d5b594c5ac97c2a9d5e5f825756fa6d359f
SyedSybtain <sybtainsyed@gmail.com>
2025-06-16 10:27:08 +0500

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div className='flex justify-between p-2 bg-black items-center max-w-screen'>
         <div className='bg-neutral-900 p-1 rounded-[50%] border-2 min-w-[50px] max-w-[50px]'>
           <Link to='/'>
-            <img className='rounded-[50%] ' src="https://lh3.googleusercontent.com/a/ACg8ocL_A23RouQyWCQWn0u75sc8Hxq1-JAK-cDEhZseZm5yreG6IV4=s288-c-no" alt="Syed Sybtain" />
+            <img className='rounded-[50%] ' src="https://avatars.githubusercontent.com/u/115772979?s=400&u=048db871df62327ae9ef3564d8e749d391219788&v=4" alt="Syed Sybtain" />
           </Link>
         </div>
         <div className='flex justify-between bg-yellow-0 min-w-[80%] p-0 items-center justify-between'>
