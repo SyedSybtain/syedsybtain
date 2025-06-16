ba9fcbf89359aa525fd3237054cf948038911a42
SyedSybtain <sybtainsyed@gmail.com>
2025-06-16 09:35:44 +0500

    border setting on hover

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,12 @@ function Projects() {
   return (
     <div className='self-center w-[90%] rounded mt-4 flex flex-col justify-center items-center gap-2 p-1 bg-opacity-100'>
         <b className='text-4xl text-black'>Demo React Projects 😀</b>
-        <div className='bg-neutral-900 p-1 w-max rounded-md border-b-2 hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/todo'>ToDo App</Link> </div>
-        <div className='bg-neutral-900 p-1 w-max rounded-md border-b-2 hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/whiteboard'>White Board</Link> </div>
-        <div className='bg-neutral-900 p-1 w-max rounded-md border-b-2 hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/stopwatch'>StopWatch</Link> </div>
-        <div className='bg-neutral-900 p-1 w-max rounded-md border-b-2 hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/digitalclock'>Digital Clock</Link> </div>
-        <div className='bg-neutral-900 p-1 w-max rounded-md border-b-2 hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/'>MyPortfolio </Link> </div>
-        <div className='bg-neutral-900 p-1 w-max rounded-md border-b-2 hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to=''>More to Add ...</Link> </div>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/todo'>ToDo App</Link> </div>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/whiteboard'>White Board</Link> </div>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/stopwatch'>StopWatch</Link> </div>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/projects/digitalclock'>Digital Clock</Link> </div>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to='/'>MyPortfolio </Link> </div>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black'><Link to=''>More to Add ...</Link> </div>
 
     </div>
   )
