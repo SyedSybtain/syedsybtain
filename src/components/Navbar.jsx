09f3dec48161958f0cc41f43d3af462cac12e0ee
SyedSybtain <sybtainsyed@gmail.com>
2025-06-17 19:51:10 +0500

    Minor Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
             <img className='rounded-full' src="https://avatars.githubusercontent.com/u/115772979?s=400&u=048db871df62327ae9ef3564d8e749d391219788&v=4" alt="Syed Sybtain" />
           </Link>
         </div>
-        <div className='flex justify-between bg-yellow-0 min-w-[80%] p-0 items-center justify-between'>
+        <div className='flex justify-between bg-yellow-0 min-w-[80%] p-0 items-center justify-between text-base md:text-2xl'>
           <div className='bg-neutral-900 p-1 rounded-md border-l-4 w-max hover:bg-white hover:text-black'><Link to='/profile'>About</Link> </div>
           <div className='bg-neutral-900 p-1 rounded-md border-l-4 w-max hover:bg-white hover:text-black'><Link to='/projects'>ReactJS Projects</Link> </div>
           <div className='bg-neutral-900 p-1 rounded-md border-l-4 w-max hover:bg-white hover:text-black'><Link to='/links'>Links</Link> </div>
