d56019f2223c5393c8a5c1b50c6c971f348548f3
SyedSybtain <sybtainsyed@gmail.com>
2025-06-24 11:39:15 +0500

    adding contact section to links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,7 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 function Links() {
   const [show,setShow] = useState(false)
+  const form = useRef()
+
+  const handleMail = async(e) => {
+    e.preventDefault()
+    try{
+      await emailjs.sendForm(
+        'service_00oru9g','template_lk3l6ua',
+        form.current,
+        {publicKey: 'NRZ3Yo8i-ftH26uN4'}
+      )
+      alert("Successful")
+    }
+    catch(error){
+      alert("Not Sent")
+    }
+  }
 
   return (
     <div className='w-[90%] text-black self-center mt-4 p-1 flex flex-col text-center'>
@@ -15,6 +32,16 @@ function Links() {
             <div>CV : <a href="https://drive.google.com/drive/folders/1zPUd-yFNVw7njvxileZMBZNGApmC5bqc?usp=drive_link">Download Link 🖇️</a> </div>
             <div>Address : KPK, Pakistan 📌 </div>
           </div>
+
+          <div className='bg-black text-white p-2 rounded-xl mt-8 w-[100%] md:w-[50%]  self-center absolute bottom-0'>
+              <b>Get In Touch</b>
+              <form className='flex flex-col gap-2' ref={form} onSubmit={handleMail}>
+                <input className='p-1 rounded' type="text" name='name' placeholder='Your Name ...'  />
+                <input className='p-1 rounded' type="email" name='email'  placeholder='Your Mail ...'  />
+                <textarea className='p-1 rounded' type="text" name='message' placeholder='Message ...' />
+                <button className='bg-white w-max p-1 rounded bg-green-600 text-white' type='submit' value='Send'>Send Email</button>
+              </form>
+            </div>
     </div>
   )
 }
