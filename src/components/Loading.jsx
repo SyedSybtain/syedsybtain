9472b0b67d5bebec34525acc5c06d4da686ed07f
SyedSybtain <sybtainsyed@gmail.com>
2025-06-30 18:47:32 +0500

    Adding Loading Popup to Main App

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='text-xl md:text-8xl flex justify-center text-black items-center h-screen'>
+        <b className='animate-bounce'>Syed Sybtain</b>
+    </div>
+  )
+}
+export default Loading
