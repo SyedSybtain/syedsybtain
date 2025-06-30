93a6374c7581b2a3e3105da9c093cd2cca93a0b2
SyedSybtain <sybtainsyed@gmail.com>
2025-06-30 18:49:17 +0500

    Adding suspense

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { lazy } from 'react'
+import { Suspense } from 'react'
+import Loading from './components/Loading'
+
+const App = lazy(()=> import("./App.jsx"))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Suspense fallback={<Loading />}>
       <App />
+    </Suspense>
     </BrowserRouter>
   </StrictMode>,
 )
