89e079e110f99707b7e3cc3a3c131cd0cf5c4d72
SyedSybtain <sybtainsyed@gmail.com>
2025-07-06 18:58:40 +0500

    Adding Loader for 3d componets

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import { Html } from '@react-three/drei'
+import React from 'react'
+
+function Loader() {
+  return (
+    <Html>
+        <div className='animate-bounce'>Loading 3D Components...Just Wait 😊 </div>
+    </Html>
+    
+  )
+}
+
+export default Loader
