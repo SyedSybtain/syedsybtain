aff9db177e88bf8a9cf1274d5938df3f330f251a
SyedSybtain <sybtainsyed@gmail.com>
2025-07-06 19:00:07 +0500

    Add Project to List

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,6 +26,7 @@ function Projects() {
   return (
     <div className='self-center w-[90%] rounded mt-4 flex flex-col justify-center items-center gap-2 p-1 bg-opacity-100'>
         <b id='project-head' className='text-sm sm:text-2xl md:text-4xl text-black flex'>Demo React Projects <FaReact id='reactLogo' className='text-blue-800'/> </b>
+        <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black project-item'><Link to='/solar3d'>3D Solar System</Link> </div>
         <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black project-item'><Link to='/projects/todo'>ToDo App</Link> </div>
         <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black project-item'><Link to='/projects/whiteboard'>White Board</Link> </div>
         <div className='bg-neutral-900 p-1 w-max rounded-md hover:bg-[#E5E5E5] hover:text-black hover:border-2 hover:border-black project-item'><Link to='/projects/stopwatch'>StopWatch</Link> </div>
